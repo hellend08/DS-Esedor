2f4fe7fbe7fcc1356a73525de4c9980cf805ecb2
DESKTOP-65F6A3A\deivy <sanchezcoronadodeivy5@gmail.com>
2021-11-08 11:17:55 -0500

    preview

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {ContactComponent} from "./views/contact/contact.component";
+import {NosotrosComponent} from "./views/nosotros/nosotros.component";
+import {ProjectsComponent} from "./views/projects/projects.component";
+import {AppComponent} from "./app.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: 'index', component: AppComponent},
+  { path: 'contacto', component: ContactComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'proyectos', component: ProjectsComponent }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProjectsComponent } from './views/projects/projects.component';
+import { NosotrosComponent } from './views/nosotros/nosotros.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    NosotrosComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
