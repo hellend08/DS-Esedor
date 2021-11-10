5d8ec6027593a2b656261d75d4562b32bc4d1a88
DESKTOP-65F6A3A\deivy <sanchezcoronadodeivy5@gmail.com>
2021-11-10 16:48:14 -0500

    services add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ export interface Empresas {
   image: string;
 }
 
+export interface Servicios {
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -20,7 +24,16 @@ export class AppComponent {
     {name: 'Primax', description: 'Clients worldwide entrust Locomotive with the design, development and management of their websites.', image: 'assets/img/empresa/primaxlogo.png'},
     {name: 'Redeban', description: 'Clients worldwide entrust Locomotive with the design, development and management of their websites.', image: 'assets/img/empresa/LogoRedeban.png'},
     {name: 'Ripley', description: 'Clients worldwide entrust Locomotive with the design, development and management of their websites.', image: 'assets/img/empresa/LogoRipley.png'},
-    {name: 'Caja Cusco', description: 'Clients worldwide entrust Locomotive with the design, development and management of their websites.', image: 'assets/img/empresa/logoCajaCusco.png'}
+    {name: 'Caja Cusco', description: 'Clients worldwide entrust Locomotive with the design, development and management of their websites.', image: 'http://www.cmac-cusco.com.pe/images/logotipo.png'}
   ];
+
+
+  dataService: Servicios[] = [
+    {title: 'Consultoría estratégica & investigación', description: 'Ayudamos a definir la estrategia para transformar tu negocio y alcanzar la madurez digital.'},
+    {title: 'Diseño de productos y servicios', description: 'Soluciones orientadas a mejorar la experiencia de tus clientes en canales digitales y presenciales.'},
+    {title: 'Sistemas de diseño y branding', description: 'Gestiona la identidad visual de tu producto digital unificanto branding, experiencia de usuario y desarrollo en un solo lugar.'},
+    {title: 'Innovación y transformación digital', description: 'Ayudamos a definir la estrategia para transformar tu negocio y alcanzar la madurez digital.\n'},
+    {title: 'Desarrollo de software y código creativo', description: 'Implementamos tecnologías emergentes desde la agilidad para generar resultados reales.'},
+  ]
 
 }
