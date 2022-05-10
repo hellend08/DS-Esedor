f9f9a3d39eaab1aad4016016c25e8734ff9e1893
ux_consultor@mdp.com.pe-at-957159725455 <sccp.hvasquez@gmail.com>
2022-05-10 16:15:33 -0500

    service en implementación

--- src/app/views/services/services.component.ts
+++ src/app/views/services/services.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as AOS from 'aos';
+
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
+  colorWhite = true;
+
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init()
   }
+
+  labelGroup = [
+    {label: 'Descubrimiento de producto'},
+    {label: 'Diseño UX/UI'},
+    {label: 'Desarrollo de software'},
+    {label: 'Sistemas de diseño'},
+    {label: 'Validación  Cuantitativa'},
+    {label: 'Transformación Cultural'}
+  ]
 
 }
